ad9faca98e9337e480e51c0ae4f58adfbc29d708
Rooholla-Khorrambakht <learn.kho@gmail.com>
2023-01-29 12:23:25 -0500

    extra build file removed

--- dockerobot-control/docker_build.sh
+++
@@ -1,3 +0,0 @@
-if [ -z "$1"] || [-z "$2"]; then echo "Wrong arguments. Usage: ./docker_build.sh image_name:tag base_image_tag" && exit 1;fi
-
-sudo docker build --force-rm --build-arg BASE_TAG=$2 -t $1 .
