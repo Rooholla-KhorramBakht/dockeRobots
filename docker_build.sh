0d14382e397a719402e16a28a86d9f5ae251aaf0
Rooholla-Khorrambakht <learn.kho@gmail.com>
2023-01-29 20:46:04 -0500

    symforce removed from base and armarch64 is added

--- docker_build.sh
+++ docker_build.sh
@@ -1,4 +1,4 @@
 if [ -z "$1"] || [-z "$2"] || [-z "$3"]; then echo "No names was provided for the image. Usage: ./docker_build.sh image_name:tag base_image:tag dockerfile_path" && exit 1;fi
 
 #Note: PY_VER and ROS1 paramters are only effective in building the dockerobot-base image
-sudo docker build --force-rm --build-arg PY_VER='3.7.0' --build-arg ROS1='true' --build-arg BASE_IMAGE=$2 -t $1 $3
+sudo docker build --force-rm --build-arg PY_VER='3.8.0' --build-arg ROS1='true' --build-arg BASE_IMAGE=$2 -t $1 $3
